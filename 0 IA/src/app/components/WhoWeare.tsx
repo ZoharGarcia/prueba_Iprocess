d38caf8b284993762060c2436dd67851be7bbe95
Zohar García <garciabrisa2007@gmail.com>
2026-02-05 10:11:50 -0600

    cambio de imagen homepage

--- 0 IA/src/app/components/WhoWeare.tsx
+++ 0 IA/src/app/components/WhoWeare.tsx
@@ -1,6 +1,6 @@
 import { Users2, CheckCircle2 } from "lucide-react";
 import { motion } from "framer-motion";
-import recursoIndustrial from "@/assets/img/Sobre Nosotros.jpeg";
+import recursoIndustrial from "@/assets/img/HomePage WhoWeAre.png";
 
 export function WhoWeAre() {
   const highlights = [
