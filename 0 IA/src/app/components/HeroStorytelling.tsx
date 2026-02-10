6ff9860ef5197e45ef449ef2eb43b591e378dba8
ZoharGarcia <garciabrisa2007@gmail.com>
2026-02-10 14:08:03 -0600

    eliminacion HeroStorytelling

--- 0 IA/src/app/components/HeroStorytelling.tsx
+++
@@ -1,7 +0,0 @@
-import { ArrowRight, CheckCircle2, Users, Globe } from "lucide-react";
-import { Button } from "@/app/components/ui/button";
-import { motion } from "motion/react";
-import { ImageWithFallback } from "@/app/components/figma/ImageWithFallback";
-import { Link } from "react-router-dom";
-import recursoPartner from "@/assets/img/PortadaUnidades.jpeg"; 
-
