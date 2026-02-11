3aef075ea80bf3c9199de67c3d32b8f2e2b46f1a
ZoharGarcia <garciabrisa2007@gmail.com>
2026-02-11 08:41:26 -0600

    Add missing imports for Header and Footer styles in main.tsx

--- Proyecto X/proyecto-x-frontend/src/main.tsx
+++ Proyecto X/proyecto-x-frontend/src/main.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
+import "./styles/Header.css";
 import "./styles/index.css";
+import "./styles/Footer.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
